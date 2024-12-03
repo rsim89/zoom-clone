c101dd89e1217c9b3ea45c53334cc0178a51c17a
rsim89 <simrok89@gmail.com>
2024-12-03 18:36:20 -0500

    commit

--- middleware.ts
+++ middleware.ts
@@ -1,17 +1,20 @@
-import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
+import { getAuth } from '@clerk/nextjs/server';
 
-const protectedRoute = createRouteMatcher([
-  '/',
-  '/upcoming',
-  '/meeting(.*)',
-  '/previous',
-  '/recordings',
-  '/personal-room',
-]);
+const protectedRoute = (path: string): boolean => {
+  const protectedRoutes = ['/', '/upcoming', '/meeting', '/previous', '/recordings', '/personal-room'];
+  return protectedRoutes.some((route) => path.startsWith(route));
+};
 
-export default clerkMiddleware((auth, req) => {
-  if (protectedRoute(req)) auth().protect();
-});
+export default async function middleware(req: Request) {
+  const url = new URL(req.url);
+  if (protectedRoute(url.pathname)) {
+    const auth = getAuth(req);
+    if (!auth.userId) {
+      return new Response('Unauthorized', { status: 401 });
+    }
+  }
+  return new Response(null, { status: 200 });
+}
 
 export const config = {
   matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
